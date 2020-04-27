27f5bfabcd3ce0ddd720ceddd90aadf2ccba5b21
VMerk <mikulenko.vladimir@yandex.ru>
2020-04-27 22:42:37 +0300

    Initial commit

---
+++ test.js
@@ -0,0 +1,35 @@
+describe('pow', function () {
+  before(() => console.log('Перед началом всех тестов'));
+  after(() => console.log('После всех тестов'));
+  beforeEach(() => console.log('Перед тестом - начинаем выполнять тест'));
+  afterEach(() => console.log('После теста - закончили выполнять тест'));
+
+  it('2 в степени 3 будет 8', function () {
+    assert.equal(pow(2, 3), 8);
+  });
+
+  it('3 в степени 3 будет 27', function () {
+    assert.equal(pow(3, 3), 27);
+  });
+
+  describe('возводит x в степень 3', function () {
+    function makeTest(x) {
+      let expected = x * x * x;
+      it(`${x} в степени 3 будет ${expected}`, function () {
+        assert.equal(pow(x, 3), expected);
+      });
+    }
+    for (let x = 1; x <= 5; x++) {
+      makeTest(x);
+    }
+  });
+
+  it('для отрицательных n возвращает NaN', function () {
+    assert.isNaN(pow(2, -1));
+  });
+
+  it('для дробных n возвращает NaN', function () {
+    assert.isNaN(pow(2, 1.5));
+  });
+
+});
